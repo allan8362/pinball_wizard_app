241169644798304c39836c9dd44741ce57cbaa1b
KevSeenan <kev.seenan@gmail.com>
2019-10-22 16:04:57 +0100

    controls guide added

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -12,6 +12,12 @@ class HomePage extends Component {
       </Switch>
       <p>Welcome to PinBall Wizard!</p>
       <p>Can you play a mean pinball?</p>
+      <p>Controls: </p>
+      <ul>
+      <li>Launch Ball: Space Bar</li>
+      <li>Left Flipper: Left Arrow Key</li>
+      <li>Right Flipper: Right Arrow Key</li>
+      </ul>
       <button><Link to="/game">New Game</Link></button>
       </Fragment>
       </Router>
